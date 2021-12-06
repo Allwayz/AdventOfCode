63820a2d9cd395c96fbef319ec5bec32191eaf94
Yuchen Liu <liuy282@cardiff.ac.uk>
2021-12-06 13:08:33 +0000

    day6AOC

--- Day5/main.py
+++ Day5/main.py
@@ -68,7 +68,6 @@ def day5(filename):
 
     G1 = defaultdict(int)
     G2 = defaultdict(int)
-
     for line in open(infile):
         start, end = line.split(' -> ')
         x1, y1 = start.split(',')
@@ -77,23 +76,55 @@ def day5(filename):
         y1 = int(y1.strip())
         x2 = int(x2.strip())
         y2 = int(y2.strip())
-
         dx = x2 - x1
         dy = y2 - y1
 
         for i in range(1 + max(abs(dx), abs(dy))):
-            x = x1 + (1 if dx > 0 else (-1 if dx < 0 else 0)) * i
+            # x = x1 + (1 if dx > 0 else (-1 if dx < 0 else 0)) * i
+            temp = 0
+            if dx > 0:
+                temp = 1
+            else:
+                if dx < 0:
+                    temp = -1
+                else:
+                    temp = 0
+            x = x1 + temp*i
             y = y1 + (1 if dy > 0 else (-1 if dy < 0 else 0)) * i
             if dx == 0 or dy == 0:
                 G1[(x, y)] += 1
             G2[(x, y)] += 1
-
     print(len([k for k in G1 if G1[k] > 1]))
     print(len([k for k in G2 if G2[k] > 1]))
+
+
+def part2(filename):
+    dotMatrix = []
+    edgeLength = 0
+    fin = open(filename)
+    readList = []
+    for row in fin.readlines():
+        temp_ = row.replace('\n', '').replace('\r', '')
+        readList.append(temp_)
+    processedList = list()
+    for element in readList:
+        pathList = []
+        subElement = element.split(" -> ", 1)
+        for item in subElement:
+            path = item.split(",", 1)
+            pathList.append((path[0], path[1]))
+        processedList.append(pathList)
+    vhList = []
+    for i in processedList:
+        for k, v in enumerate(i):
+            # if k == 0:
+            # if i[k][0] == i[k+1][0]
+            print(k, v)
 
 
 if __name__ == '__main__':
     script_dir = os.path.dirname(os.path.abspath(__file__))
     input_file = Path(script_dir, '.', 'puzzleInput.in')
     test_input_file = Path(script_dir, '.', 'testInput.in')
-    day5(input_file)
+    day5(test_input_file)
+    # part2(test_input_file)
